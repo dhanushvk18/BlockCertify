699bb3fd654be4fde61ae9f67d213b501851bb87
dhanushvk18 <153421454+dhanushvk18@users.noreply.github.com>
2026-04-26 18:05:13 +0530

    added sss character for testing

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-
+ssss
 const artifact = JSON.parse(
   fs.readFileSync(
     "./artifacts/contracts/DocumentVerification.sol/DocumentVerification.json",
